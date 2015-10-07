47bfcd024f7e4aa1e3ee0e37535559b03558bfbe
Nicolas Carenton <nicolas.carenton@ipsl.jussieu.fr>
2015-10-07 16:20:20 +0200

    Custom mediator.py for ciclad

--- sudospawner/mediator.py
+++ sudospawner/mediator.py
@@ -20,13 +20,14 @@ import json
 import pipes
 import os
 import sys
+from string import Template
 
 from subprocess import Popen
+import subprocess
 
 from tornado import log
 from tornado.options import parse_command_line
 app_log = log.app_log
-
 
 def finish(data, fp=sys.stdout):
     """write JSON to stdout"""
@@ -34,20 +35,38 @@ def finish(data, fp=sys.stdout):
     app_log.debug("mediator result: %s", data)
     sys.stdout.flush()
 
-
 def kill(pid, signal):
     """send a signal to a PID"""
     app_log.debug("Sending signal %i to %i", signal, pid)
-    try:
-        os.kill(pid, signal)
-    except OSError as e:
-        if e.errno == errno.ESRCH:
-            # not running
-            alive = False
+
+    if (signal == 0):
+        p = Popen(["qstat", str(pid)],
+            cwd=os.path.expanduser('~'),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        streamdata = p.communicate()[0]
+        rc = p.returncode
+        if (rc == 0):
+            alive = True
         else:
-            raise
-    else:
-        alive = True
+            alive = False
+
+    if (signal in [2, 9, 15]):
+        p = Popen(["qdel", str(pid)],
+            cwd=os.path.expanduser('~'),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+
+        streamdata = p.communicate()[0]
+        rc = p.returncode
+        if (rc == 0):
+            alive = False
+
     finish({'alive': alive})
 
 def spawn(args, env):
@@ -57,13 +76,54 @@ def spawn(args, env):
     Start the single-user server via `python -m jupyterhub.singleuser`,
     and prohibit PYTHONPATH from env for basic protections.
     """
+
+    # Extracting port from given args    
+    for line in args:
+        if "--port=" in line:
+            port = line.split("=")[1]
+
+    serialpbs = Template('''
+#PBS -S /bin/bash
+##PBS -l nodes=1:ppn=1,walltime=$hours:00:00,pvmem=${mem}gb
+#PBS -l walltime=$hours:00:00
+#PBS -N $id
+#PBS -q $queue
+#PBS -r n
+#PBS -o /tmp/s_$id.log
+#PBS -j oe
+#PBS -V
+
+export PATH=/prodigfs/jupyter/anaconda3/envs/virtualenv/bin:$PATH
+export VIRTUAL_ENV=/prodigfs/jupyter/anaconda3/envs/virtualenv/
+
+# setup tunnel for notebook
+ssh -N -f -R $port:localhost:$port jupyter-test.ipsl.upmc.fr
+# setup tunnel for API
+ssh -N -f -L 8081:localhost:8081 jupyter-test.ipsl.upmc.fr
+
+    ''')
+    queue = "short"
+    mem = 1
+    hours = 1
+    id = "jup"
+    serialpbs = serialpbs.substitute(dict(queue = queue, mem = mem, hours=hours,  id=id, port=port, PATH="$PATH"))
+    serialpbs+='\n'
+    #serialpbs+='cd %s' % "notebooks"
+    serialpbs+='\n'
+    #cmd+=' > /tmp/jupyter.log 2>&1'
+
     cmd = [sys.executable, '-m', 'jupyterhub.singleuser'] + args
-    cmd_s = ' '.join(pipes.quote(s) for s in cmd)
+    for k in ["JPY_API_TOKEN"]:
+        cmd.insert(0, "export %s='%s';" % (k, env[k]))
+
+    #cmd_s = ' '.join(pipes.quote(s) for s in cmd)
+    cmd_s = ' '.join(cmd)
+    serialpbs+=cmd_s
     app_log.info("Spawning %s", cmd_s)
     if 'PYTHONPATH' in env:
         app_log.warn("PYTHONPATH env not allowed for security reasons")
         env.pop('PYTHONPATH')
-    
+   
     # use fork to prevent zombie process
     # create pipe to get PID from descendant
     r, w = os.pipe()
@@ -74,31 +134,33 @@ def spawn(args, env):
         sys.stdout.write(r.read())
     else:
         os.close(r)
-        
         # don't inherit signals from Hub
         os.setpgrp()
-        
         # detach child FDs, to allow parent process to exit while child waits
         null = os.open(os.devnull, os.O_RDWR)
         for fp in [sys.stdin, sys.stdout, sys.stderr]:
             os.dup2(null, fp.fileno())
         os.close(null)
-        
+         
         # launch the single-user server from the subprocess
         # TODO: If we want to see single-user log output,
         # we should send stderr to a file
-        p = Popen(cmd, env=env,
+        p = Popen('qsub', env=env,
             cwd=os.path.expanduser('~'),
-            stdout=open(os.devnull, 'w'),
+            stdin=subprocess.PIPE,
+            stdout=subprocess.PIPE,
         )
+
+        out = p.communicate(serialpbs.encode())[0].strip()
+        jobid = out.decode('ascii').split('.')[0]
+
         # pipe finish message to parent
         w = os.fdopen(w, 'w')
-        finish({'pid': p.pid}, w)
+        finish({'pid': int(jobid)}, w)
         w.close()
         
         # wait for subprocess, so it doesn't get zombified
         p.wait()
-
 
 def main():
     """parse JSON from stdin, and take the appropriate action"""
